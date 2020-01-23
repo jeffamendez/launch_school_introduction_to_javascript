62d4855eef66d9aac958ecd194147ed9c003d426
Jeff Mendez <jeffamendez@gmail.com>
2020-01-24 07:53:23 +0800

    finished greeter file for variable

---
+++ Introduction to Programming with JavaScript/Chapter_1/Variables/greeter.js
@@ -0,0 +1,6 @@
+let firstName = 'Victor';
+console.log(`Good Morning, ${firstName}.`);
+console.log(`Good Afternoon, ${firstName}.`);
+console.log(`Good Evening, ${firstName}.`);
+
+console.log('Good Morning, ' + firstName + '.' );
